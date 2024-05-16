cc3a63814ca6dd94ef3cc2f048e77e174dbc08e6
itskrisverse <vestigesweety@gmail.com>
2024-05-17 05:17:51 +0530

    New file

---
+++ Numberguess.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main () {
+
+    int num , guess , tries=0;
+
+    srand (time (NULL));
+    num = ((rand () % 100)) + 1;
+
+    cout <<"*************** NUMBER GUESSING GAME ***************\n";
+
+    do {
+        cout << "Enter a guess between ( 1- 100 ) : ";
+        cin >> guess;
+        tries++;
+
+        if( guess > num ) {
+            cout << "Too high!\n";
+        }
+        else if( guess < num ) {
+            cout << "Too low!\n";
+        }
+        else {
+            cout << "CORRECT! , Number of tries : "<< tries << endl;
+        }
+
+    }
+    while (guess != num);
+
+    cout << "**************************************************\n";
+
+    return 0;
+}
